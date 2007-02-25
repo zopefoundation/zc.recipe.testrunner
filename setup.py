7dc6a684beda13d036485208980ebdcac95f885c
tlotze <tlotze@62d5b8a3-27da-0310-9561-8e5933582275>
2007-02-25 21:35:46 +0000

    bump version to 1.0.0b6
    git-svn-id: file:///Users/mac/vcs/main/zc.recipe.testrunner/trunk@72805 62d5b8a3-27da-0310-9561-8e5933582275

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(*rnames):
 name = "zc.recipe.testrunner"
 setup(
     name = name,
-    version = "1.0.0b5",
+    version = "1.0.0b6",
     author = "Jim Fulton",
     author_email = "jim@zope.com",
     description = "ZC Buildout recipe for creating test runners",
