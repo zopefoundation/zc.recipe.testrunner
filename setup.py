1b2d22ca9f8567363ca1a61bb1fec120078dbb01
srichter <srichter@62d5b8a3-27da-0310-9561-8e5933582275>
2010-06-09 20:20:06 +0000

    svb
    git-svn-id: file:///Users/mac/vcs/main/zc.recipe.testrunner/trunk@113305 62d5b8a3-27da-0310-9561-8e5933582275

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(*rnames):
 name = "zc.recipe.testrunner"
 setup(
     name = name,
-    version = "1.3.0",
+    version = "1.4.0dev",
     author = "Jim Fulton",
     author_email = "jim@zope.com",
     description = "ZC Buildout recipe for creating test runners",
